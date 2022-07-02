a7b22ff8998c2b52a2107dbbb2f77e684548b4d3
Omar Carrasco <omarcarrascoguerrero@gmail.com>
2022-07-02 09:25:35 -0500

    mejoradando all

---
+++ codigo.js
@@ -0,0 +1,18 @@
+
+// const nav = document.querySelector('.menuNav')
+
+// window.addEventListener('scroll',()=>{
+//     let ubicacion = window.pageYOffset;
+    
+    
+//     if(ubicacion>=700){
+//         let ubicacionTwo = window.pageYOffset;
+//         if(ubicacion>=ubicacionTwo){
+//             nav.style.top = "0px";
+//         }
+//         else{
+//             nav.style.top = "-60px";
+//         }
+//         ubicacion =ubicacionTwo
+//     } 
+// });
